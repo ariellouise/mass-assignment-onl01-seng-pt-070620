37fb6f4258bafe20788d9f31b5c101c620e364db
ariel murphy <arielladders@gmail.com>
2020-08-04 20:05:03 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,7 @@
 class Person
  attr_accessor :name, :birthday, :eye_color, :hair_color, :weight, :height, :handed, :complexion, :t_shirt_size, :pant_length, :pant_width, :wrist_size, :glove_size
+ 
+ def initialize(attributes)
+   attributes.each {|key, value| self.send(("#{key}="), value)}
+ end 
 end
