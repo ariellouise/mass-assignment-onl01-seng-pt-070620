40be3142638cb86e073ad796f7794e9ef1cc613e
ariel murphy <arielladders@gmail.com>
2020-08-04 20:03:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,3 @@
 class Person
- attr_accessor :name, :birthday, :eye_color, :hair_color, 
+ attr_accessor :name, :birthday, :eye_color, :hair_color, :weight, :height, :handed, :complexion, :t_shirt_size
 end
